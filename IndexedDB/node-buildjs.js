93cc2e3dd8946a083e8189c95033821fd18ca147
Brett Zamir <brettz9@yahoo.com>
2016-07-06 14:12:19 -0700

    Only report unknown scripts

--- IndexedDB/node-buildjs.js
+++ IndexedDB/node-buildjs.js
@@ -3,9 +3,13 @@ Array.prototype.includes = function(item) {
 };
 
 const cheerio = require('cheerio');
-
 const fs = require('fs');
 const path = require('path');
+
+// Known scripts
+const testHarnessScripts = ['/resources/testharness.js', '/resources/testharnessreport.js'];
+const supportAndTestHarnessScripts = testHarnessScripts.concat('support.js');
+const webIDLScripts = supportAndTestHarnessScripts.concat('/resources/WebIDLParser.js', '/resources/idlharness.js');
 
 const dirPath = process.argv[2] || './';
 fs.readdir(dirPath, function(err, items) {
@@ -25,14 +29,20 @@ fs.readdir(dirPath, function(err, items) {
       $ = cheerio.load(data);
 
       // List files without standard 3 items
-      if ($('script[src]').length !== 3) {
-        console.log('Non-typical file for script src count ' + $('script[src]').length +  ': ' + htmlFile);
+      var scriptCount = $('script[src]').length;
+      if (scriptCount !== 3) {
+//        console.log('Non-typical file for script src count ' + scriptCount +  ': ' + htmlFile);
       }
 
       $('script[src]').each(function (script, item) {
         const src = $(this).attr('src');
-        if (!['support.js', '/resources/testharness.js',
-          '/resources/testharnessreport.js'].includes(src)) {
+        if (scriptCount === 3 && !supportAndTestHarnessScripts.includes(src)) {
+          console.log('Found non-typical script src: ' + src + ' in: ' + htmlFile);
+        }
+        else if (scriptCount === 2 && !testHarnessScripts.includes(src)) {
+          console.log('Found non-typical script src (out of 2): ' + src + ' in: ' + htmlFile)
+        }
+        else if ((scriptCount < 2 || scriptCount > 3) && !webIDLScripts.includes(src)) {
           console.log('Found non-typical script src: ' + src + ' in: ' + htmlFile);
         }
       });
