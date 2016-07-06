fda24ecded97c6b2da8a635e287d072bd32c6514
Brett Zamir <brettz9@yahoo.com>
2016-07-06 14:01:35 -0700

    - List non-typical script tag counts and files; - Report only after all files saved

--- IndexedDB/node-buildjs.js
+++ IndexedDB/node-buildjs.js
@@ -1,3 +1,7 @@
+Array.prototype.includes = function(item) {
+    return this.indexOf(item) > -1;
+};
+
 const cheerio = require('cheerio');
 
 const fs = require('fs');
@@ -7,9 +11,10 @@ const dirPath = process.argv[2] || './';
 fs.readdir(dirPath, function(err, items) {
   const htmlExt = /\.html?$/;
   const htmlFiles = items.filter((item) => item.match(htmlExt));
+  var ct = 0;
 
   // Iterate IndexedDB files
-  htmlFiles.forEach((htmlFile) => {
+  htmlFiles.forEach((htmlFile, i) => {
     const jsFile = htmlFile.replace(htmlExt, '.js');
     const outputFile = path.join('js', jsFile);
 
@@ -18,9 +23,26 @@ fs.readdir(dirPath, function(err, items) {
 
       // Extract JavaScript content and save to file
       $ = cheerio.load(data);
+
+      // List files without standard 3 items
+      if ($('script[src]').length !== 3) {
+        console.log('Non-typical file for script src count ' + $('script[src]').length +  ': ' + htmlFile);
+      }
+
+      $('script[src]').each(function (script, item) {
+        const src = $(this).attr('src');
+        if (!['support.js', '/resources/testharness.js',
+          '/resources/testharnessreport.js'].includes(src)) {
+          console.log('Found non-typical script src: ' + src + ' in: ' + htmlFile);
+        }
+      });
       fs.writeFile(outputFile, $('script').text(), function(err) {
-          if (err) {return console.log(err);}
-          console.log("The file " + outputFile + " was saved!");
+        ct++;
+        if (err) {return console.log(err);}
+        // console.log("The file " + outputFile + " was saved!");
+        if (ct === htmlFiles.length - 1) {
+          console.log('All files have been saved!');
+        }
       });
     });
   });
